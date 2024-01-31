fd08d44e39ee7091eb37ba5d0fa55fe8e742e6ce
kasinathan28 <way2kasinathb35@gmail.com>
2024-01-31 23:18:51 +0530

    Profile page is not working on the real device when apk builded

--- my_app/lib/screens/Profile/profile.dart
+++ my_app/lib/screens/Profile/profile.dart
@@ -2,9 +2,11 @@ import "package:flutter/material.dart";
 import 'package:my_app/screens/Profile/components/body.dart';
 
 class Profile extends StatelessWidget {
+  const Profile({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Body(),
     );
   }
